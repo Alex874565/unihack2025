107ab65444664a8263f9d24dda80ce2ca5245d01
Alex874565 <alex.carpinisan@gmail.com>
2025-11-15 17:04:54 +0200

    Place vfx and logic

--- Assets/Scripts/Managers/EnvironmentManager.cs
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -29,7 +29,6 @@ public class EnvironmentManager : MonoBehaviour
         foreach (Animator grassAnimator in _grassAnimators)
         {
             grassAnimator.SetFloat("SoilPollution", ServiceLocator.Instance.PollutionManager.SoilPollutionLevel);
-            Debug.Log("EnvironmentManager - Update: Setting SoilPollution to " + ServiceLocator.Instance.PollutionManager.SoilPollutionLevel);
             _cloudsAnimator.SetFloat("AirPollution", ServiceLocator.Instance.PollutionManager.AirPollutionLevel);
         }
     }
