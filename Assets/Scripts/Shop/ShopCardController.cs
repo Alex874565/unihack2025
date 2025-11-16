408e0b6cad9db240d09a045094e047e57af6052b
unknown <kellerroxana30@gmail.com>
2025-11-16 09:34:45 +0200

    ui improvements

--- Assets/Scripts/Shop/ShopCardController.cs
+++ Assets/Scripts/Shop/ShopCardController.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class ShopCardController : MonoBehaviour
 {
+    private AudioManager audioManager;
+    private bool hasHovered = false; // optional: avoid playing repeatedly
+
+    private void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
     public void ScaleUp()
     {
         gameObject.transform.localScale *= 1.1f;
+
+        // Play hover sound once per hover
+        if (!hasHovered && audioManager != null && audioManager.hover != null)
+        {
+            audioManager.PlaySFX(audioManager.hover);
+            hasHovered = true;
+        }
     }
 
     public void ScaleDown()
     {
         gameObject.transform.localScale = new Vector3(6f, 10f, 10f);
+        hasHovered = false; // reset so it can play next time
     }
 }
